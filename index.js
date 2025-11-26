7dd04e7082ff0da29de54322c51700ed627680f1
mahamudul-alam <alam.mmahamudul@gmail.com>
2025-11-26 13:24:16 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.port || 5000
+
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nma65uq.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    
+  await client.connect(); 
+    
+    const db = client.db('bookDB');
+    const courseCollection = db.collection('courses')
+   
+
+/* all books using GET request */
+    app.get('/courses', async (req, res)=>{
+        const result = await courseCollection.find().toArray();
+      
+
+        res.send(result)
+
+    })
+
+app.get('/courses/:id', async (req, res)=>{
+
+    const {id} = req.params;
+    const result = await courseCollection.findOne({_id: new ObjectId(id)});
+
+    res.send(result)
+
+})
+
+
+/* add book using POST request */
+
+app.post('/courses', async (req, res) =>{
+    const newCourse = req.body;
+    
+    const result = await courseCollection.insertOne(newCourse)
+    res.send(result)
+})
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+    /* await client.close(); */
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
